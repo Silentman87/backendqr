32da1843cfddaf15ccedea60a3c2798b1bf17335
shravan <choudharyshravan8965@gmail.com>
2025-06-01 16:29:51 +0530

    first commit

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyparser = require('body-parser');
-const db = require('../db');
+const connectDB = require('../db');
 const serverless  = require('serverless-http');
 require('dotenv').config();
 
@@ -9,6 +9,9 @@ const userRouter = require('../UserRoute');
 const User = require('../Model/User');
 
 const app = express();
+
+
+connectDB();
 
 app.use(cors({
      origin: '*',
--- db.js
+++ db.js
@@ -1,13 +1,16 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 
-const mongoURI = process.env.DB_URL;
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('✅ MongoDB connected');
+  } catch (err) {
+    console.error('❌ MongoDB connection error:', err);
+    process.exit(1); // Crash if DB fails
+  }
+};
 
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('Error connecting to MongoDB:', err));
-
-module.exports = mongoose;
+module.exports = connectDB;
