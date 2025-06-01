2179d181fafc970d0cd27cdea943542d8f6ab002
shravan <choudharyshravan8965@gmail.com>
2025-06-01 15:03:08 +0530

    fix3

--- api/index.js
+++ api/index.js
@@ -6,31 +6,20 @@ const serverless  = require('serverless-http');
 require('dotenv').config();
 
 const userRouter = require('../UserRoute');
-
 const User = require('../Model/User');
- // if it's in models/User.js
-
 
 const app = express();
 
-
 app.use(cors({
-     origin: '*', // correct origin
+     origin: '*',
 }));
- // handle preflight requests
 
 app.use(bodyparser.json());
 
-
 app.use('/testuser', userRouter);
-
 
 app.get('/hello', (req, res) => {
   res.json({ message: "Hello from Vercel!" });
 });
 
-
-
-
-module.exports.handler = serverless(app)
-
+module.exports = serverless(app);  // default export the handler
